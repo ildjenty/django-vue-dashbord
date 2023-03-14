edb2fcc69bff52ce12ca192408d7ca71a43fab22
s_takamagari <s_takamagari@localhost.localdomain>
2023-03-14 10:00:07 +0900

    REMVOE:不要コメント

--- src/server/constants/base.py
+++ src/server/constants/base.py
@@ -4,7 +4,6 @@ class ConstantError(Exception):
 
 class ConstantMeta(type):
     def __new__(meta_class, classname, bases, dict):
-        # 異なるConstantMetaを継承していないか検証する
         sub_classes = [cls for cls in bases if isinstance(cls, ConstantMeta)]
         for sub_cls in sub_classes[1:]:
             if sub_classes[0] != sub_cls:
@@ -12,7 +11,6 @@ class ConstantMeta(type):
                     f"Can't inhelitance of [{sub_classes[0].__name__}] and [{sub_cls.__name__}] together"
                 )
 
-        # 親クラス同士で定数の衝突が起こっていないか確認
         super_constants = set()
         for base in bases:
             base_constants = ConstantMeta.__get_constant_attr(meta_class, base.__dict__)
@@ -22,29 +20,22 @@ class ConstantMeta(type):
                 raise ConstantError(f"Collision the constant [{collisions_str}]")
             super_constants |= base_constants
 
-        # 定義するクラスで定数の再定義をしていないか確認
         new_constants = ConstantMeta.__get_constant_attr(meta_class, dict)
         rebinds = super_constants & new_constants
         if rebinds:
             rebinds_str = ", ".join(rebinds)
             raise ConstantError(f"Can't rebind constant [{rebinds_str}]")
 
-        # __init__関数置き換えてインスタンス生成を禁止する
         def _meta__init__(self, *args, **kwargs):
-            # インスタンスの生成をしようとした際、ConstantErrorを送出する。
             raise ConstantError("Can't make instance of Constant class")
 
-        # __init__関数を置き換えてインスタンス生成を禁止する。
         dict["__init__"] = _meta__init__
 
         return type.__new__(meta_class, classname, bases, dict)
 
     @staticmethod
     def __get_constant_attr(meta_class, dict):
-        # 特殊なアトリビュートを除くアトリビュートを取得する
         attrs = set(atr for atr in dict if not ConstantMeta.__is_special_func(atr))
-        # アトリビュートがすべて定数または例外的にクラス変数に格納可能な
-        # 変数であることを確認する
         cnst_atr = set(atr for atr in attrs if meta_class.is_constant_attr(atr))
         var_atr = set(atr for atr in attrs if meta_class.is_settable_attr(atr))
         wrong_atr = attrs - (cnst_atr | var_atr)
